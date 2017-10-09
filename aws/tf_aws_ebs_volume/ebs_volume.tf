ae30bf6cabc968e04077919e5172d621255c0bc2
appav28 <chad_kellerman@troweprice.com>
2017-10-09 08:13:25 -0400

    ebs volume tweaks

--- aws/tf_aws_ebs_volume/ebs_volume.tf
+++ aws/tf_aws_ebs_volume/ebs_volume.tf
@@ -5,10 +5,10 @@ data "aws_caller_identity" "current" {}
 # --- https://www.terraform.io/docs/providers/aws/r/ebs_volume.html
 resource "aws_ebs_volume" "create_ebs_module" {
     encrypted         = "true"
-    type              = "${var.mod_ebs_type}"
-    size              = "${var.mod_ebs_size}"
-    snapshot_id       = "${var.mod_ebs_use_snap}"
-    availability_zone = "${var.mod_ebs_az}"
+    type              = "${var.ebs_volume_type}"
+    size              = "${var.ebs_volume_size}"
+    snapshot_id       = "${var.ebs_volume_snap_id}"
+    availability_zone = "${var.ebs_volme_az}"
     tags {
       account_id = "${data.aws_caller_identity.current.account_id}"
       caller_arn = "${data.aws_caller_identity.current.arn}"
