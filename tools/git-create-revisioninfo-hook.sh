b893073a6765f71a74142154e83163940c8218f8
Chad Kellerman <sunckell@gmail.com>
2017-10-10 19:29:33 -0400

    tooling update

--- tools/git-create-revisioninfo-hook.sh
+++ tools/git-create-revisioninfo-hook.sh
@@ -5,10 +5,11 @@
 # --- Example:
 # ---   [master]v2.0.0-beta-191(a830382)
 # --- Install:
-# ---  cp git-create-revisioninfo-hook.sh .git/hooks/post-commit
-# ---  cp git-create-revisioninfo-hook.sh .git/hooks/post-checkout
-# ---  cp git-create-revisioninfo-hook.sh .git/hooks/post-merge
+# ---  cp tools/git-create-revisioninfo-hook.sh .git/hooks/pre-commit
+# ---  cp tools/git-create-revisioninfo-hook.sh .git/hooks/post-checkout
+# ---  cp tools/git-create-revisioninfo-hook.sh .git/hooks/post-merge
 # ---  chmod +x .git/hooks/post-*
+# ---  chmod +x .git/hooks/pre-*
 
 FILENAME='gitrevision'
 
@@ -16,7 +17,11 @@ exec 1>&2
 branch=`git rev-parse --abbrev-ref HEAD`
 shorthash=`git log --pretty=format:'%h' -n 1`
 revcount=`git log --oneline | wc -l`
+
 latesttag=`git describe --tags --abbrev=0`
+if [ $? -ne 0 ]; then
+  latesttag="none"
+fi
 
 VERSION="[$branch]$latesttag-$revcount($shorthash)"
 echo ${VERSION} > ${FILENAME}
